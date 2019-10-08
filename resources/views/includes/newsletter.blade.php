d0b18dcccff63a840d8c4bdf297cdaadfac07c1f
Xavir <xaviranik.me@gmail.com>
2019-10-09 01:24:38 +0600

    Added: Includes blade, Tweaked UI

---
+++ resources/views/includes/newsletter.blade.php
@@ -0,0 +1,25 @@
+<div class="newsletter">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="newsletter_border"></div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-8 offset-lg-2">
+                <div class="newsletter_content text-center">
+                    <div class="newsletter_title">Subscribe to our newsletter</div>
+                    <div class="newsletter_text">
+                        <p>Tell us a little about yourself below to gain access today:</p>
+                    </div>
+                    <div class="newsletter_form_container">
+                        <form action="#" id="newsletter_form" class="newsletter_form">
+                            <input type="email" class="newsletter_input" required="required">
+                            <button class="newsletter_button trans_200"><span>Subscribe</span></button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/layouts/front.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Vonit Technology</title>
+    <link rel="stylesheet" type="text/css" href="{{ asset('theme/styles/bootstrap4/bootstrap.min.css') }}">
+    <link href="{{ asset('theme/plugins/font-awesome-4.7.0/css/font-awesome.min.css') }}" rel="stylesheet"
+        type="text/css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('theme/plugins/OwlCarousel2-2.2.1/owl.carousel.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('theme/plugins/OwlCarousel2-2.2.1/owl.theme.default.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('theme/plugins/OwlCarousel2-2.2.1/animate.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('theme/styles/main_styles.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('theme/styles/responsive.css') }}">
+
+</head>
+
+<body>
+    <div class="super_container">
+        @include('includes.header')
+
+        @yield('content')
+        <!-- Footer -->
+        @include('includes.footer')
+    </div>
+    <script src="{{ asset('theme/js/jquery-3.2.1.min.js') }}"></script>
+    <script src="{{ asset('theme/styles/bootstrap4/popper.js') }}"></script>
+    <script src="{{ asset('theme/styles/bootstrap4/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('theme/plugins/greensock/TweenMax.min.js') }}"></script>
+    <script src="{{ asset('theme/plugins/greensock/TimelineMax.min.js') }}"></script>
+    <script src="{{ asset('theme/plugins/scrollmagic/ScrollMagic.min.js') }}"></script>
+    <script src="{{ asset('theme/plugins/greensock/animation.gsap.min.js') }}"></script>
+    <script src="{{ asset('theme/plugins/greensock/ScrollToPlugin.min.js') }}"></script>
+    <script src="{{ asset('theme/plugins/OwlCarousel2-2.2.1/owl.carousel.js') }}"></script>
+    <script src="{{ asset('theme/plugins/Isotope/isotope.pkgd.min.js') }}"></script>
+    <script src="{{ asset('theme/plugins/easing/easing.js') }}"></script>
+    <script src="{{ asset('theme/plugins/parallax-js-master/parallax.min.js') }}"></script>
+    <script src="{{ asset('theme/js/custom.js') }}"></script>
+</body>
+
+</html>
