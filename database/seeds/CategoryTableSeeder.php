2990c0096ef0e44adf903b0fe306ecd8811c3232
Xavir <xaviranik.me@gmail.com>
2019-10-09 23:09:38 +0600

    Added: CategoryTableSeeder

---
+++ database/seeds/CategoryTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Category;
+use Illuminate\Database\Seeder;
+
+class CategoryTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $category_1 = [
+            'name' => 'Laptops',
+        ];
+        $category_2 = [
+            'name' => 'Motherboards',
+        ];
+        $category_3 = [
+            'name' => 'SSD',
+        ];
+        $category_4 = [
+            'name' => 'Laptop RAMS',
+        ];
+
+        Category::create($category_1);
+        Category::create($category_2);
+        Category::create($category_3);
+        Category::create($category_4);
+        
+    }
+}
